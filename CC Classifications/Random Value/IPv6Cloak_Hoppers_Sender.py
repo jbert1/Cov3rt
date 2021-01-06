0926a5b8fa84e599b9b5224e90b7f34027a42e2f
daniel-munger <daniel.lloyd.munger@gmail.com>
2021-01-06 15:23:38 -0600

    Mine don't work yet

---
+++ CC Classifications/Random Value/IPv6Cloak_Hoppers_Sender.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+
+IP_DST = "1234:5678:9001:1337:ABCD:0000:FACE:AAAA"
+
+# Collects user input to determine content of message to send over the covert channel.
+secret_phrase = input("Enter your Secret Phrase:")
+
+secret_phrase_bin = ''.join((format(ord(i), 'b').zfill(8) for i in secret_phrase))
+
+
+p = IPv6()
