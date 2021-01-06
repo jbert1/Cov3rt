c287ddb0c71daa749480a5d0c533e6702933cea0
daniel-munger <daniel.lloyd.munger@gmail.com>
2021-01-06 16:28:22 -0600

    I want to commit after writing that

--- CC Classifications/Random Value/IPv6Cloak_Hoppers_Sender.py
+++ CC Classifications/Random Value/IPv6Cloak_Hoppers_Sender.py
@@ -5,7 +5,21 @@ IP_DST = "1234:5678:9001:1337:ABCD:0000:FACE:AAAA"
 # Collects user input to determine content of message to send over the covert channel.
 secret_phrase = input("Enter your Secret Phrase:")
 
-secret_phrase_bin = ''.join((format(ord(i), 'b').zfill(8) for i in secret_phrase))
+secret_phrase_dec = [ord(i) for i in secret_phrase]
 
+print(secret_phrase_dec)
 
 ls(IPv6)
+p = IPv6(src = 'fe80::1461:beca:7ad:3167', dst = 'fe80::1461:beca:7ad:3167')
+
+p.hlim = 69
+send(p)
+
+for c in secret_phrase_dec:
+    p.hlim = c
+    print(c)
+    ls(IPv6)
+    send(p)
+
+p.hlim = 69
+send(p)
