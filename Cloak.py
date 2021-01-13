b93b31d8b04a5c3e370ae821b69a1e64bbec8965
jbert1 <jmb100898@gmail.com>
2021-01-13 15:26:37 -0600

    How does this look?

--- Cloak.py
+++ Cloak.py
@@ -4,18 +4,16 @@ from abc import ABC, abstractmethod
 
 # Cloak superclass
 class Cloak(ABC):
-    
+
     @abstractmethod
     def ingest(self, data):
         """Ingests and formats data for the cloak to communicate."""
         pass
     
-    @abstractmethod
     def send_EOT(self):
         """Sends an end-of-transmission packet to signal the end of transmission."""
         pass
 
-    @abstractmethod
     def send_delimiter(self):
         """Sends a delimiter to signal the end of a specified data stream."""
         pass
@@ -38,7 +36,6 @@ class Cloak(ABC):
     
     # Setter for 'description'
     @description.setter
-    @abstractmethod
     def description(self, d):
         # Check type
         if isinstance(d, str):
@@ -46,11 +43,27 @@ class Cloak(ABC):
             self._description = d
         else:
             error("'description' must be of type 'str'")
+    
+    # Getter for 'name'
+    @property
+    def name(self):
+        return self._name
+    
+    # Setter for 'name'
+    @name.setter
+    def name(self, d):
+        # Check type
+        if isinstance(d, str):
+            # Set the name
+            self._name = d
+        else:
+            error("'name' must be of type 'str'")
 
 class TestCloak(Cloak):
 
-    def __init__(self, description = "An example cloak for the cov3rt framework."):
+    def __init__(self, description = "An example cloak for the cov3rt framework.", name = "Test Cloak"):
         self.description = description
+        self.name = name
     
     def ingest(self, data):
         """Ingests and formats data for the cloak to communicate."""
