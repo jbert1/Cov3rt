55b23d6b459cfe950f5a8b63e0b19c7a2a8ecaa2
jbert <jbert@localhost.localdomain>
2021-01-13 15:08:12 -0600

    I want description to work like I want it to

--- Cloak.py
+++ Cloak.py
@@ -1,15 +1,9 @@
 from scapy.all import send
 from logging import critical, error, info, warning, debug
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 # Cloak superclass
-class Cloak:
-
-    # Constructor
-    @abstractmethod
-    def __init__(self, description = "An example cloak for the cov3rt framework."):
-        # Description for this cloak
-        self.desc = description
+class Cloak(ABC):
     
     @abstractmethod
     def ingest(self, data):
@@ -52,3 +46,28 @@ class Cloak:
             self._description = d
         else:
             error("'description' must be of type 'str'")
+
+class TestCloak(Cloak):
+
+    def __init__(self, description = "An example cloak for the cov3rt framework."):
+        self.description = description
+    
+    def ingest(self, data):
+        """Ingests and formats data for the cloak to communicate."""
+        pass
+    
+    def send_EOT(self):
+        """Sends an end-of-transmission packet to signal the end of transmission."""
+        pass
+
+    def send_delimiter(self):
+        """Sends a delimiter to signal the end of a specified data stream."""
+        pass
+
+    def send_packets(self):
+        """Sends the entire ingested data via the send_packet method."""
+        pass
+
+    def send_packet(self, data):
+        """Sends packet(s) via a defined covert channel."""
+        pass
