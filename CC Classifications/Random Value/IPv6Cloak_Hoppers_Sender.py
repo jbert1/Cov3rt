89258dfd62656cf20d748bfda904882720be41d6
daniel-munger <daniel.lloyd.munger@gmail.com>
2021-01-15 09:10:59 -0600

    Life is meaningless

--- CC Classifications/Random Value/IPv6Cloak_Hoppers_Sender.py
+++ CC Classifications/Random Value/IPv6Cloak_Hoppers_Sender.py
@@ -1,25 +1,41 @@
 from scapy.all import *
 
-IP_DST = "1234:5678:9001:1337:ABCD:0000:FACE:AAAA"
+starting_hl = 68
 
-# Collects user input to determine content of message to send over the covert channel.
+# Collects user input to determine content of message to send over the covert channel
 secret_phrase = input("Enter your Secret Phrase:")
 
+# Retrieves ASCII Value for every character in secret phrase
 secret_phrase_dec = [ord(i) for i in secret_phrase]
 
-print(secret_phrase_dec)
+# print(secret_phrase_dec) DEBUG
 
-ls(IPv6)
-p = IPv6(src = 'fe80::1461:beca:7ad:3167', dst = 'fe80::1461:beca:7ad:3167')
+# Creates an IPv6 Packet 
+p = IPv6(src = 'fe80::1461:beca:7ad:3167', dst = 'ff02::1:ffad:317')
 
-p.hlim = 69
+# Sets Hop Limit to transmit at start and end of phrase transmission
+p.hlim = starting_hl
+
+# ls(p) DEBUG
+
+# Sends packet
 send(p)
 
+# For each character ...
 for c in secret_phrase_dec:
-    p.hlim = c
-    print(c)
-    ls(IPv6)
+    # Set Hop Limit to ASCII character value + initial hop limit
+    p.hlim = c + starting_hl 
+    
+
+    # print(c) DEBUG
+    # ls(p) DEBUG
+    
+    # Send the packet
     send(p)
 
-p.hlim = 69
+# Sets Hop Limit to transmit at start and end of phrase transmission
+p.hlim = starting_hl
+
+# ls(p) DEBUG
+
 send(p)
