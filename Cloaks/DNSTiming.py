bbfc98a2be2e9ad2672c685ac31476ed3a564e0a
Rwarcards762 <christopherrice9@gmail.com>
2021-01-24 07:15:31 +0000

    Finished implementation of DNSTiming channel; still has some cleaning up to do but is functional as of now (I think)

--- Cloaks/DNSTiming.py
+++ Cloaks/DNSTiming.py
@@ -3,8 +3,9 @@ from scapy.layers.dns import DNS, DNSQR
 from scapy.layers.inet import IP, UDP
 
 from Cloak import Cloak
-from logging import error
+from logging import error, info
 from re import search
+from time import sleep
 
 '''Options:
 IP_DST
@@ -19,7 +20,7 @@ class DNSTiming(Cloak):
     # Regular expression to verify IP
     IP_REGEX = "^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9 \][0-9]?)$"
 
-    def __init__(self, description = "A cloak based on delays between DNS requests to domains.", name="DNS Timing", ip_dst="8.8.8.8", DOMAIN_DELIM, DOMAIN_CONT, ZERO_TIMING, ONE_TIMING):
+    def __init__(self, description = "A cloak based on delays between DNS requests to domains.", name="DNS Timing", ip_dst="8.8.8.8", DOMAIN_DELIM="wikipedia.org", DOMAIN_CONT="twitter.com", ZERO_TIMING=2, ONE_TIMING=10):
         self.description = description
         self.name = name
         self.ip_dst = ip_dst
@@ -27,12 +28,12 @@ class DNSTiming(Cloak):
         self.domaincont = DOMAIN_CONT + "."
         self.zerotiming = ZERO_TIMING
         self.onetiming = ONE_TIMING
-        self.read_data = ""
+        self.read_data = []
 
     def ingest(self, data):
         '''Ingests and formats data as a binary stream.'''
         if isinstance(data, str):
-            self.data = ''.join(format(ord(i), 'b').zfill(8) for i in user_input)
+            self.data = ''.join(format(ord(i), 'b').zfill(8) for i in data)
         else:
             error("'data' must be of type 'str'")
 
@@ -46,15 +47,19 @@ class DNSTiming(Cloak):
         if databit == '0':
             sleep(self.zerotiming)
             pkt = IP(dst=self.ip_dst)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname = self.domaincont.capitalize()))
+            send(pkt, verbose=False)
         elif databit == '1':
             sleep(self.onetiming)
             pkt = IP(dst=self.ip_dst)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname = self.domaincont.capitalize()))
+            send(pkt, verbose=False)
 
     def send_packets(self):
         '''Sends ingested data via the send_packet method.'''
+        print("Sending data:")
+        print(self.data)
         # Send an initial packet in order to start a baseline for delays.
         initpkt = IP(dst=self.ip_dst)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname = self.domaincont.capitalize()))
-        self.send_packet(initpkt)
+        send(initpkt, verbose=False)
         # Sends actual data.
         for item in self.data:
             self.send_packet(item)
@@ -65,10 +70,11 @@ class DNSTiming(Cloak):
     def packet_handler(self, pkt):
         '''Specifies the packet handler for receiving info via the DNS Timing Cloak.'''
         if (pkt.haslayer(IP) and pkt.haslayer(UDP) and pkt.haslayer(DNS) and pkt.haslayer(DNSQR)):
-            if (pkt["IP"].dst == self.ip_dst and pkt["DNS"].rd == 1 and pkt["DNSQR"].qname.lower() == self.domain.lower().encode()):
-                pass # Not sure what I really need for this function, as my data processing
-                # for the packets is entirely handled in recv_packets()
-
+            if (pkt["IP"].dst == self.ip_dst and pkt["DNS"].rd == 1 and pkt["DNSQR"].qname.lower() == self.domaindelim.lower().encode()):
+                self.read_data.append(pkt)
+            elif (pkt["IP"].dst == self.ip_dst and pkt["DNS"].rd == 1 and pkt["DNSQR"].qname.lower() == self.domaincont.lower().encode()):
+                self.read_data.append(pkt)
+        
     def recv_EOT(self, pkt):
         '''Specifies the EOT packet, singaling the end of transmission.'''
         if (pkt.haslayer(IP) and pkt.haslayer(UDP) and pkt.haslayer(DNS) and pkt.haslayer(DNSQR)):
@@ -79,21 +85,24 @@ class DNSTiming(Cloak):
 
     def recv_packets(self, timeout = None, max_count = None, iface = None):
         '''Receives packets which use the DNS Timing Cloak.'''
-        self.read_data = ""
+        self.read_data = []
+        print("Sniffing beginning...")
         sniff(timeout = timeout, stop_filter = self.recv_EOT, prn = self.packet_handler)
         # Decode the data collected, based on timings between packets
+        print("Sniffing has ended, EOT received")
         string = ''
         current_time = None
         prev_time = None
         pktdif = 0
-        # Loop over our data
-        for item in range(0, len(self.read_data), 8):
+        # Loop over our data, ignoring last packet as it does not contain "data"
+        for item in self.read_data[:-1]:
             # Set the prev_time to the last updated current_time (as it is now one behind)
             prev_time = current_time
             # Now, replace current_time with data from packet
-            current_time = item[UDP].time
+            current_time = item.time
             # Compare difference, unless this is the first packet
             if prev_time == None:
+                print("First packet ignored")
                 continue
             # Otherwise, this means we have a valid prev_time and can take delays
             pktdif = current_time - prev_time
@@ -107,14 +116,18 @@ class DNSTiming(Cloak):
                 string = string + '0'
             else:
                 string = string + '1'
-        
+        print("Data has been reconverted to binary, output is")
+        print(string)
         # Once our string has been populated, we can recreate the original data by reconverting
         output_string = ""
         # List Comp to split our input into groups of 8 for easy decoding
-        split_input = [string[i:i+n] for i in range(0, len(string), 8)]
+        split_input = [string[i:i+8] for i in range(0, len(string), 8)]
+        print("Split input is")
+        print(split_input)
         # Iterate through groups of 8, creating output data
         for item in split_input:
-            asciichar = chr(int(item,2))
+            bin_num = "0b{}".format(item)
+            asciichar = chr(int(bin_num,2))
             output_string += asciichar
         
         return output_string
