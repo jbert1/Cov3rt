05ce02fd1f348d7a62e0e82fd290bd26816aea82
jbert1 <jmb100898@gmail.com>
2021-01-27 17:15:40 -0600

    Menu option for selecting cloaks

--- interactive.py
+++ interactive.py
@@ -2,6 +2,11 @@
 # encoding: utf-8
 
 import npyscreen
+from importlib import import_module
+from inspect import getmembers, isclass
+from os import listdir
+from os import name as OS_NAME
+from cov3rt.Cloaks import Cloak
 
 ### TEST CODE (Mainly kept for quick reference) ###
 # class ActionFormObject(npyscreen.ActionForm, npyscreen.FormWithMenus):
@@ -85,97 +90,103 @@ import npyscreen
 # if __name__ == "__main__":
 #     app = App().run()
 
-# Classification encoding
-INTER_PACKET_TIMING = "Inter-Packet Timing"
-MESSAGE_TIMING = "Message Timing"
-RATE_THROUGHPUT_TIMING = "Rate/Throughput"
-ARTIFICIAL_LOSS = "Artificial Loss"
-MESSAGE_ORDERING = "Message (PDU) Ordering"
-RETRANSMISSION = "Retransmission"
-FRAME_COLLISIONS = "Frame Collisions"
-TEMPERATURE = "Temperature"
-SIZE_MODULATION = "Size Modulation"
-POSITION = "Sequence: Position"
-NUMBER_OF_ELEMENTS = "Sequence: Number of Elements"
-RANDOM_VALUE = "Random Value"
-CASE_MODULATION = "Value Modulation: Case"
-LSB_MODULATION = "Value Modulation: LSB"
-VALUE_INFLUENCING = "Value Modulation: Value Influencing"
-RESERVED_UNUSED = "Reserved/Unused"
-PAYLOAD_FIELD_SIZE_MODULATION = "Payload Field Size Modulation"
-USER_DATA_CORRUPTION = "User-Data Corruption"
-MODIFY_REDUNDANCY = "Modify Redundancy"
-USER_DATA_VALUE_MODULATION_RESERVED_UNUSED = "User-Data Value Modulation & Reserved/Unused" 
-
 
 # This is my idea for storing cloak classifications
 cloaks =  {
-    INTER_PACKET_TIMING : [
-        "DNSTiming",
+    Cloak.Cloak.INTER_PACKET_TIMING : [
     ],
 
-    MESSAGE_TIMING : [
+    Cloak.Cloak.MESSAGE_TIMING : [
     ],
 
-    RATE_THROUGHPUT_TIMING : [
+    Cloak.Cloak.RATE_THROUGHPUT_TIMING : [
     ],
 
-    ARTIFICIAL_LOSS : [
+    Cloak.Cloak.ARTIFICIAL_LOSS : [
     ],
 
-    MESSAGE_ORDERING : [
+    Cloak.Cloak.MESSAGE_ORDERING : [
     ],
 
-    RETRANSMISSION : [
+    Cloak.Cloak.RETRANSMISSION : [
     ],
 
-    FRAME_COLLISIONS : [
+    Cloak.Cloak.FRAME_COLLISIONS : [
     ],
     
-    TEMPERATURE : [
+    Cloak.Cloak.TEMPERATURE : [
     ],
 
-    SIZE_MODULATION : [
-        "UDPRaw",
+    Cloak.Cloak.SIZE_MODULATION : [
     ],
 
-    POSITION : [
+    Cloak.Cloak.POSITION : [
     ],
 
-    NUMBER_OF_ELEMENTS : [
+    Cloak.Cloak.NUMBER_OF_ELEMENTS : [
     ],
 
-    RANDOM_VALUE : [
-        "IPv6Hoppers","TCP",
+    Cloak.Cloak.RANDOM_VALUE : [
     ],
 
-    CASE_MODULATION : [
-        "DNSCaseModulation"
+    Cloak.Cloak.CASE_MODULATION : [
     ],
 
-    LSB_MODULATION : [
+    Cloak.Cloak.LSB_MODULATION : [
     ],
     
-    VALUE_INFLUENCING : [
+    Cloak.Cloak.VALUE_INFLUENCING : [
     ],
 
-    RESERVED_UNUSED : [
-        "IPReservedBit"
+    Cloak.Cloak.RESERVED_UNUSED : [
     ],
     
-    PAYLOAD_FIELD_SIZE_MODULATION : [
+    Cloak.Cloak.PAYLOAD_FIELD_SIZE_MODULATION : [
     ],
 
-    USER_DATA_CORRUPTION : [
+    Cloak.Cloak.USER_DATA_CORRUPTION : [
     ],
 
-    MODIFY_REDUNDANCY : [
+    Cloak.Cloak.MODIFY_REDUNDANCY : [
     ],
 
-    USER_DATA_VALUE_MODULATION_RESERVED_UNUSED : [
+    Cloak.Cloak.USER_DATA_VALUE_MODULATION_RESERVED_UNUSED : [
     ],
 }
 
+# from cov3rt.Cloaks.DNSTiming import DNSTiming
+# cloaks =  {
+#     Cloak.Cloak.INTER_PACKET_TIMING : [
+#         DNSTiming
+#     ],
+# }
+
+# Add classes from files in a folder with a defined package name
+def add_classes(filepath, package_name):
+    # Get all of the cloaks within the folder
+    files = listdir(filepath)
+    # Loop over the filenames
+    for filename in files:
+        # Ignore these files and accept only python files
+        if (filename not in ["__init__.py", "__pycache__", "Cloak.py"]) and (filename[-3:] == ".py"):
+            # Grab the module name
+            module_name = package_name + '.' + filename[:-3]
+            # Get each class name and class in the file
+            for classname, cls in getmembers(import_module(module_name), isclass):
+                # Try-catch for odd imports
+                try:
+                    # Create the class import path
+                    module_path = "{}.{}.{}".format(package_name, classname, classname)
+                    # Get the class object path
+                    imprt = str(cls).split("'")[1]
+                    # Compare the paths and ignore the "Cloak" import
+                    if (module_path == imprt) and (classname != "Cloak"):
+                        # Check for the classification
+                        if cloaks.get(cls.classification, -1) != -1:
+                            # Add to the classification
+                            cloaks[cls.classification].append(cls)
+                except:
+                    pass
 
 # Main application
 class App(npyscreen.NPSAppManaged):
@@ -184,13 +195,13 @@ class App(npyscreen.NPSAppManaged):
         self.addForm("MAIN", 
             HomePage, 
             name = "Welcome to cov3rt",
-            lines = 20,
+            lines = 22,
             columns = 80
         )
         self.addForm("CloakSelection",
             Second_TUI,
             name = "Cloak Selection",
-            lines = 20,
+            lines = 22,
             columns = 80
         )
 
@@ -199,8 +210,9 @@ class HomePage(npyscreen.ActionForm, npyscreen.FormWithMenus):
 
     # Defines the elements on the page
     def create(self):
+        self.nextrely -= 1
         # Header
-        self.header = self.add(npyscreen.Pager, relx = 20, color = "DANGER", editable = False, height = 6,
+        self.header = self.add(npyscreen.Pager, relx = 20, color = "DANGER", editable = False, height = 5,
             values = [
                 "                 ╭───╮       │",
                 "           ╱╲_╱╲     │      ─┼──",
@@ -211,54 +223,58 @@ class HomePage(npyscreen.ActionForm, npyscreen.FormWithMenus):
 
         )
         self.nextrely += 2
-        # List of cloaks
-        self.cloak_classification = self.add(npyscreen.TitleSelectOne, scroll_exit = True, begin_entry_at = 25, max_height = 7,
-            name = "Cloak Classifications:", 
-            values = list(cloaks.keys())
+        self.disclaimer = self.add(npyscreen.Pager, height = 5, relx = 5, editable = False,
+            values = [
+                "This tool should only be used to enhance the effectiveness of",
+                "communication policy. You should not misuse this tool to gain",
+                "access into computer systems or to circumvent communication policy",
+                "on networks you do not own. We are not responsible for any direct",
+                "or indirect damages caused due to the improper usage of this tool."
+            ]
         )
-        
+        self.nextrely += 1
+        self.cloak_classification = self.add(npyscreen.TitleFixedText, relx = 5, begin_entry_at = 18, editable = False,
+            name = "Classification:",
+            value = ""
+        )
+
+        self.cloak_type = self.add(npyscreen.TitleFixedText, relx = 5, begin_entry_at = 18, editable = False,
+            name = "Name:",
+            value = ""
+        )
+
+        self.cloak_description = self.add(npyscreen.TitlePager, relx = 5, begin_entry_at = 18, editable = False,
+            name = "Description:",
+            values = ["Press CTRL+X to open the menu."]
+        )
+
+        # Create a menu to store cloaks
         self.menu = self.new_menu(name = "Cloak Selection",
-            shortcut = 'm'
+            shortcut = '^X'
         )
-        self.menu.addItem("Item 1", self.press_1, "1")
-        self.menu.addItem("Item 2", self.press_2, "2")
+        # Loop over the cloak classifications
+        for cloak_classification in cloaks:
+            # Temporarily store the submenu to populate it
+            submenu = self.menu.addNewSubmenu(cloak_classification)
+            # Loop over each cloak
+            for cloak in cloaks[cloak_classification]:
+                # Add it to the submenu
+                submenu.addItem(
+                    text = cloak.name, 
+                    onSelect = self.populateScreen,
+                    arguments = [cloak]
+                )
+            # Add close menu at the bottom for convenience
+            submenu.addItem("Close Menu", self.close_menu, "^X")
+        # Add close menu at the bottom for convenience
         self.menu.addItem("Close Menu", self.close_menu, "^X")
+    
+    def populateScreen(self, cloak):
+        self.cloak = cloak
+        self.cloak_classification.value = cloak.classification
+        self.cloak_type.value = cloak.name
+        self.cloak_description.values = cloak.description.split("\n")
         
-        for cloak_name in cloaks:
-            with self.menu.addNewSubmenu(cloak_name) as submenu:
-                for cloak in cloaks[cloak_name]:
-                    submenu.addItem(cloak.name, cloak_options_next_page(cloak))
-        
-        def cloak_options_next_page(self, cloak):
-            pass
-
-
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu = self.menu.addNewSubmenu("A sub menu!", 's')
-        # self.submenu10 = self.menu.addNewSubmenu("A sub menu!", 's')
-
-        self.populate(self.submenu10)
-
-        self.submenu.addItem("Close Menu", self.close_menu, "^X")
-
-    #def populate(self, submenu):
-      
-        #for 
-        #submenu.addItem()
-
-    def press_1(self):
-        npyscreen.notify_confirm("You pressed Item 1!", "Item 1", editw=1)
-    def press_2(self):
-        npyscreen.notify_confirm("You pressed Item 2!", "Item 2", editw=1)
     def close_menu(self):
         self.parentApp.setNextForm(None)
 
@@ -306,11 +322,15 @@ class Second_TUI(npyscreen.ActionForm):
 
 # Main program
 if __name__ == '__main__':
+
+    # Get path for cov3rt
+    if OS_NAME == "nt":
+        # Windows path
+        COV3RT_PATH = "\\".join(Cloak.__file__.split("\\")[:-1])
+    else:
+        COV3RT_PATH = '/'.join(Cloak.__file__.split('/')[:-1])
+
+    add_classes(COV3RT_PATH, "cov3rt.Cloaks")
+
     app = App().run()
-    print("""
-                 ╭───╮       │
-           ╱╲_╱╲     │      ─┼──
-╭─── ╭───╮ ╲╷ ╷╱  ───┤ ╭───╮ │  
-│    │   │  ╲_╱      │ │     │  
-╰─── ╰───╯   ╳   ╰───╯ ╵     ╰──
-""")
+    
