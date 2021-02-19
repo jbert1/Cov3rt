7d3c9c139ba69625bd6624f2f5d6d48919af7bf5
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-02-18 22:27:04 -0500

    Updating setup

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, setuptools
 
 setup(
     name='cov3rt',
-    version='0.0.5',
+    version='0.0.6',
     description='Testing Install of cov3rt',
     url='https://github.com/jbert1/Cov3rt',
-    author='auth',
+    author='Justin Berthelot Sam Dominguez Daniel Munger Christopher Rice',
     author_email='author@email.com',
     license='MIT',
     packages=setuptools.find_packages(),
-    zip_safe=False,
+    install_requires = ['scapy>=2.4.3','npyscreen']
+    zip_safe=False
 )
