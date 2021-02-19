2b33686b18a004d96f0025fcf7cfcb81c34fbee9
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-02-18 20:38:07 -0500

    Adding cov3rt token and setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, setuptools
+
+setup(
+    name='cov3rt',
+    version='0.0.4',
+    description='Testing Install of cov3rt',
+    url='https://github.com/jbert1/Cov3rt',
+    author='auth',
+    author_email='author@email.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
