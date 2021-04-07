2494de9a99c9ab0150fa71be74fd600c278d2f2a
daniel-munger <daniel.lloyd.munger@gmail.com>
2021-04-07 14:53:52 -0500

    .. / .... .- - . / -- --- .-. ... . / -.-. --- -.. . / -. --- .-- / .- -. -.. / .. .----. -- / -. . ...- . .-. / - --- ..- -.-. .... .. -. --. / .. - / .- --. .- .. -. / --- -.- / -... -.-- .

--- cov3rt/Cloaks/IPMorse.py
+++ cov3rt/Cloaks/IPMorse.py
@@ -8,6 +8,7 @@ from scapy.utils import wrpcap
 from time import sleep
 from cov3rt.Cloaks.Cloak import Cloak
 
+""" The Morse Code Dictionary used in this cloak"""
 morse_code = { 'A':'.-', 'B':'-...', 
                     'C':'-.-.', 'D':'-..', 'E':'.', 
                     'F':'..-.', 'G':'--.', 'H':'....', 
@@ -24,6 +25,20 @@ morse_code = { 'A':'.-', 'B':'-...',
                     '?':'..--..', '/':'-..-.', '-':'-....-', 
                     '(':'-.--.', ')':'-.--.-'}
 
+reverse_morse = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+                    '..-.': 'F', '--.': 'G', '....': 'H',
+                    '..': 'I', '.---': 'J', '-.-': 'K',
+                    '.-..': 'L', '--': 'M', '-.': 'N',
+                    '---': 'O', '.--.': 'P', '--.-': 'Q',
+                    '.-.': 'R', '...': 'S', '-': 'T',
+                    '..-': 'U', '...-': 'V', '.--': 'W',
+                    '-..-': 'X', '-.--': 'Y', '--..': 'Z',
+                    '.----': '1', '..---': '2', '...--': '3',
+                    '....-': '4', '.....': '5', '-....': '6',
+                    '--...': '7', '---..': '8', '----.': '9',
+                    '-----': '0', '--..--': ', ', '.-.-.-': '.',
+                    '..--..': '?', '-..-.': '/', '-....-': '-',
+                    '-.--.': '(', '-.--.-': ')', ' ': ' '}
 
 class IPMorse(Cloak):
 
@@ -39,7 +54,7 @@ class IPMorse(Cloak):
         self.ip_dst = ip_dst
         self.send_port = send_port
         self.dest_port = dest_port
-        self.read_data = []
+        self.read_data = ""
 
 
     def ingest(self, data):
@@ -60,7 +75,7 @@ class IPMorse(Cloak):
 
     def send_EOT(self, iface=None):
         """Sends an end-of-transmission packet to signal the end of transmission."""
-        packet_string = urandom(1738)
+        packet_string = urandom(679)
         pkt = IP(dst=self.ip_dst) / UDP(sport=self.send_port, dport=self.dest_port) / Raw(packet_string)  
         if self.LOGLEVEL == DEBUG:
             send(pkt, verbose=True, iface=iface)
@@ -71,7 +86,6 @@ class IPMorse(Cloak):
         """Sends delimiter packet to signify end of Morse Code character. """
         packet_string = urandom(42)
         pkt = IP(dst=self.ip_dst) / UDP(sport=self.send_port, dport=self.dest_port) / Raw(packet_string)
-    
         if self.LOGLEVEL == DEBUG:
             send(pkt, verbose=True, iface=iface)
         else:
@@ -108,54 +122,78 @@ class IPMorse(Cloak):
     def send_packets(self, iface=None, packetDelay=None, delimitDelay=None, endDelay=None):
         """Sends the entire ingested data via the send_packet method."""
         info("Sending packets...")
+        
         # Loop over the data
         for item in self.data:
             for char in item:
                 self.send_packet(char, iface)
+                if isinstance(packetDelay, int) or isinstance(packetDelay, float):
+                    debug("Packet delay sleep for {}s".format(packetDelay))
+                    sleep(packetDelay)
+            if isinstance(delimitDelay, int) or isinstance(delimitDelay, float):
+                debug("Delimit delay sleep for {}s".format(delimitDelay))
+                sleep(delimitDelay)
             self.send_delimiter(iface)
-            # Packet delay
-            if isinstance(packetDelay, int) or isinstance(packetDelay, float):
-                debug("Packet delay sleep for {}s".format(packetDelay))
-                sleep(packetDelay)
-
+        
         # End delay
         if isinstance(endDelay, int) or isinstance(endDelay, float):
             debug("End delay sleep for {}s".format(endDelay))
-            sleep(endDelay)
+            sleep(endDelay) 
         self.send_EOT(iface)
+
         return True
 
     def packet_handler(self, pkt):
         """Specifies the packet handler for receiving information via the IP Identifiaction Cloak."""
-        if pkt.haslayer(IP):
-            if pkt["IP"].dst == self.ip_dst:
-                self.read_data.append(pkt.id)
-                debug("Received {}".format(pkt.id))
+        if pkt.haslayer(UDP) and pkt.haslayer(IP):
+            # Check for UDP packets with correct ports
+            if pkt["IP"].dst == self.ip_dst and pkt["UDP"].sport == self.send_port and pkt["UDP"].dport == self.dest_port:
+                    # Decipher character
+                    if pkt.haslayer(Raw):
+                        length = len(pkt[Raw].load) 
+                        if length == 42:
+                            self.read_data += "a"
+                        elif length == 1337:
+                            self.read_data += " "   
+                    else:
+                        if pkt["IP"].flags == 0x00:
+                            self.read_data += "."
+                        elif pkt["IP"].flags == 0x04:
+                            self.read_data += "-"
+                    info("Received {}".format(self.read_data))
 
     def recv_EOT(self, pkt):
-        """Specifies the end-of-transmission packet that signals the end of transmission."""
-        if pkt.haslayer(IP):
-            if pkt["IP"].dst == self.ip_dst and pkt["IP"].id == self.EOT_ID:
-                info("Received EOT")
-                return True
+        '''Specifies the EOT packet, signaling the end of transmission.'''
+        if pkt.haslayer(UDP) and pkt.haslayer(IP) and pkt.haslayer(Raw):
+            # Check for correct options
+            if pkt["IP"].dst == self.ip_dst and pkt["UDP"].sport == self.send_port and pkt["UDP"].dport == self.dest_port:
+                print("b")
+                length = len(pkt["Raw"].load)
+                print(length)
+                if length == 679:
+                    print("c")
+                    info("Received EOT")
+                    return True
         return False
 
     def recv_packets(self, timeout=None, max_count=None, iface=None, in_file=None, out_file=None):
-        """Receives packets which use the IP Identification Morse Code Cloak."""
+        """Receives packets which use the IP Morse Code Cloak in dots and dashes, then converts dots and dashes to cleartext"""
         info("Receiving packets...")
-        self.read_data = []
+        self.read_data = ""
         if max_count:
             packets = sniff(timeout=timeout, count=max_count, iface=iface, offline=in_file, stop_filter=self.recv_EOT, prn=self.packet_handler)
         else:
             packets = sniff(timeout=timeout, iface=iface, offline=in_file, stop_filter=self.recv_EOT, prn=self.packet_handler)
         if out_file:
             wrpcap(out_file, packets)
+
+        print(self.read_data.split("a")[:-1])
         # Decode read data
         decoded_string = ''
         # Loop over the data
-        for item in self.read_data[:-1]:
-            # Get the ascii character and add it to our string
-            decoded_string += chr(item)
+        for item in self.read_data.split("a")[:-1]:
+            decoded_string += reverse_morse[item]            
+
         info("String decoded: {}".format(decoded_string))
         return decoded_string
 
